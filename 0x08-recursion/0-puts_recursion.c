6117e07cc485cdacc5b1cc52db3f2b4adca38900
sam jilowa <samueljilowa@gmail.com>
2023-04-06 16:24:32 -0700

    a program that prints the string followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Print
+ a string followed by a new line 
+ * @s: string
+ * Author: Samuel jilowa
+ * Country: zambia
+ *Email: samueljilowa@gmail.com
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
