b4e3a39e4b8e36461e381bdc700b4d62ae0fa8c5
sam jilowa <samueljilowa@gmail.com>
2023-04-10 06:33:46 -0700

    a function that prints a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print a string in reverse mode 
+ * Author: samuel jilowa
+ * Country:  Zambia
+ * Email: samueljilowa@gmail.com
+ * School  ALX school of swe
+ * @s: string to be printed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(1 +s);
+	_putchar(*s);
+}
