fbe366823f9e4cc7c285a13be221dabd4dfd124b
samjilowa <samueljilowa@gmail.com.com>
2023-10-06 15:24:13 -0700

    updated

--- 0x15-file_io/0-read_text_file.c
+++ 0x15-file_io/0-read_text_file.c
@@ -1,48 +1,44 @@
+#include <stdlib.h>
 #include "main.h"
-
-/*
- * read_textfile - Reads a text file and prints it to POSIX stdout
- * @filename: The name of the file to read
- * @letters: The number of letters to read and print
- * Return: The number of letters read and printed, or 0 if there's an error
+/**
+ * read_textfile - reads a text file and prints it
+ * @filename: file to be read
+ * @letters: the number of letters function should read and print
+ *
+ * Return: Number of letters it could read & print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;            /* File descriptor */
-    ssize_t rcount, wcount;  /* Read and write count */
-    char *buffer;      /* Buffer to store read data */
+	int file_descriptor;
+	int read_bytes;
+	int printed_bytes;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
-
-    /* Open the file for reading and writing */
-    fd = open(filename, O_RDWR);
-    if (fd == -1)
-        return (0);
-
-    /* Allocate memory for the buffer */
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-    {
-        free(buffer);
-        return (0);
-    }
-
-    /* Read data from the file into the buffer */
-    rcount = read(fd, buffer, letters);
-    if (rcount == -1)
-        return (0);
-
-    /* Write the read data to stdout */
-    wcount = write(STDOUT_FILENO, buffer, rcount);
-    if (wcount == -1 || rcount != wcount)
-        return (0);
-
-    /* Free the allocated buffer */
-    free(buffer);
-
-    /* Close the file */
-    close(fd);
-
-    return (wcount); /* Return the number of letters read and printed */
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+	{
+		return (0);
+	}
+	read_bytes = read(file_descriptor, buffer, letters);
+	if (read_bytes == -1)
+	{
+		return (0);
+	}
+	printed_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+	if (printed_bytes == -1)
+	{
+		return (0);
+	}
+	free(buffer);
+	close(file_descriptor);
+	return (printed_bytes);
 }
