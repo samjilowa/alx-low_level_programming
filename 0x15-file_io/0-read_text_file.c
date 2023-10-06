136fb42a94623aab9b26bfdcb72fbcfcd9df842c
samjilowa <samueljilowa@gmail.com.com>
2023-10-06 15:16:47 -0700

    updated

--- 0x15-file_io/0-read_text_file.c
+++ 0x15-file_io/0-read_text_file.c
@@ -1,39 +1,48 @@
 #include "main.h"
 
-/**
- * read_textfile - Read a text file and print to POSIX stdout
- * @filename: char string of files name
- * @letters: number of letters to read and print
- * Return: number of letters read and printed, or 0 if error
+/*
+ * read_textfile - Reads a text file and prints it to POSIX stdout
+ * @filename: The name of the file to read
+ * @letters: The number of letters to read and print
+ * Return: The number of letters read and printed, or 0 if there's an error
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t rcount, wcount;
-	char *buffer;
+    int fd;            /* File descriptor */
+    ssize_t rcount, wcount;  /* Read and write count */
+    char *buffer;      /* Buffer to store read data */
 
-	if (filename == NULL)
-		return (0);
+    if (filename == NULL)
+        return (0);
 
-	fd = open(filename, O_RDWR);
-	if (fd == -1)
-		return (0);
+    /* Open the file for reading and writing */
+    fd = open(filename, O_RDWR);
+    if (fd == -1)
+        return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		free(buffer);
-		return (0);
-	}
-	rcount = read(fd, buffer, letters);
-	if (rcount == -1)
-		return (0);
+    /* Allocate memory for the buffer */
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+    {
+        free(buffer);
+        return (0);
+    }
 
-	wcount = write(STDOUT_FILENO, buffer, rcount);
-	if (wcount == -1 || rcount != wcount)
-		return (0);
-	free(buffer);
+    /* Read data from the file into the buffer */
+    rcount = read(fd, buffer, letters);
+    if (rcount == -1)
+        return (0);
 
-	close(fd);
-	return (wcount);
+    /* Write the read data to stdout */
+    wcount = write(STDOUT_FILENO, buffer, rcount);
+    if (wcount == -1 || rcount != wcount)
+        return (0);
+
+    /* Free the allocated buffer */
+    free(buffer);
+
+    /* Close the file */
+    close(fd);
+
+    return (wcount); /* Return the number of letters read and printed */
 }
