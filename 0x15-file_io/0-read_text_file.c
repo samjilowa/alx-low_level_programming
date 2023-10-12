10871ac671ada68ae0b1a071338d18914a982287
samjilowa <54450360+samjilowa@users.noreply.github.com>
2023-10-12 23:00:26 +0300

    Updated 0-read_text_file.c

--- 0x15-file_io/0-read_text_file.c
+++ 0x15-file_io/0-read_text_file.c
@@ -1,44 +1,39 @@
+#include <stddef.h>
 #include <stdlib.h>
 #include "main.h"
+
+
 /**
- * read_textfile - reads a text file and prints it
- * @filename: file to be read
- * @letters: the number of letters function should read and print
+ * read_textfile - read a text file to certain size
+ * @filename: the file pointer to readfrom
+ * @letters: the number of letters you want to read
  *
- * Return: Number of letters it could read & print
+ * Return: void
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor;
-	int read_bytes;
-	int printed_bytes;
 	char *buffer;
+	ssize_t o, r, w;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
+
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
+		free(buffer);
 		return (0);
 	}
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
-	{
-		return (0);
-	}
-	read_bytes = read(file_descriptor, buffer, letters);
-	if (read_bytes == -1)
-	{
-		return (0);
-	}
-	printed_bytes = write(STDOUT_FILENO, buffer, read_bytes);
-	if (printed_bytes == -1)
-	{
-		return (0);
-	}
+
 	free(buffer);
-	close(file_descriptor);
-	return (printed_bytes);
+	close(o);
+
+	return (w);
 }
